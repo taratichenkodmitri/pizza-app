24e032aa3b369937f3b65b49cf3611cf42eb8333
dmitriy.tarataichenko <dmitriy.tarataichenko@elecard.ru>
2024-08-04 21:23:13 +0700

    [+] add Button appearance

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,14 @@ import cn from 'classnames';
 import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 
-const Button: FC<ButtonProps> = ({ children, ...props }) => {
+const Button: FC<ButtonProps> = ({ children, appearance = 'small', ...props }) => {
   return (
     <button
       {...props}
-      className={cn(styles.Button, styles.Accent)}
+      className={cn(styles.Button, styles.Accent, {
+        [styles.Small]: appearance === 'small',
+        [styles.Big]: appearance === 'big',
+      })}
     >
       {children}
     </button>
