2fab20ce6521d9284ad4e697dfdaa046f9c90a22
dmitriy.tarataichenko <dmitriy.tarataichenko@elecard.ru>
2024-08-12 01:07:24 +0700

    [+] Sidebar

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,11 @@ import cn from 'classnames';
 import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 
-const Button: FC<ButtonProps> = ({ children, appearance = 'small', ...props }) => {
+const Button: FC<ButtonProps> = ({ children, className, appearance = 'small', ...props }) => {
   return (
     <button
       {...props}
-      className={cn(styles.Button, styles.Accent, {
+      className={cn(styles.Button, styles.Accent, className, {
         [styles.Small]: appearance === 'small',
         [styles.Big]: appearance === 'big',
       })}
--- src/layout/MenuLayout/MenuLayout.tsx
+++ src/layout/MenuLayout/MenuLayout.tsx
@@ -3,13 +3,50 @@ import cn from 'classnames';
 import styles from './MenuLayout.module.css';
 import { MenuLayoutProps } from './MenuLayout.props';
 import { Link, Outlet } from 'react-router-dom';
+import Button from '../../components/Button/Button';
 
 const MenuLayout: FC<MenuLayoutProps> = () => {
   return (
     <div className={cn(styles.MenuLayout)}>
-      <div>
-        <Link to="/">Menu</Link>
-        <Link to="/cart">Cart</Link>
+      <div className={cn(styles.Sidebar)}>
+        <div className={styles.User}>
+          <img
+            className={styles.Avatar}
+            src="avatar.png"
+            alt="Avatar"
+          />
+          <div className={styles.Username}>User name</div>
+          <div className={styles.Email}>user@user.com</div>
+        </div>
+        <div className={styles.Menu}>
+          <Link
+            to="/"
+            className={styles.Link}
+          >
+            <img
+              src="menu-icon.svg"
+              alt="Menu icon"
+            ></img>
+            Menu
+          </Link>
+          <Link
+            to="/cart"
+            className={styles.Link}
+          >
+            <img
+              src="cart-icon.svg"
+              alt="Cart icon"
+            ></img>
+            Cart
+          </Link>
+        </div>
+        <Button className={styles.Exit}>
+          <img
+            src="exit-icon.svg"
+            alt="Exit icon"
+          />
+          Exit
+        </Button>
       </div>
       <div>
         <Outlet />
